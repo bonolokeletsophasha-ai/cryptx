a2ef09d58f1482c42d363b7df1d8597099566526
bonolokeletsophasha-ai <bonolokeletsophasha@gmail.com>
2026-09-20 00:33:17 +0200

    Update App.jsx -Importing from Sidebar -Importing from Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
+import Sidebar from './components/;ayout/Sidebar'
+import Header from './components/layout/Header'
+
 function App() {
-  return (
-    <div className="min-h-screen bg-purple-600 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-white">
-        CryptX
-      </h1>
+  return ( 
+  <main className="min-h-screen bg-{#858585] p-0 lg:p-4">
+    <div className="mx-auto flex min-h-screen max-w-[1400px] overflow-hidden bg-white lg:min-h-[calc(100vh-2rem)] lg:rounded-2xl">
+
+    /* Sidebar */ 
+    <Sidebar/>
+
+    /* Main Area*/ 
+    <section className="min-w-0 flex-1">
+
+      /* Header */
+      <header/>
+
+      /* Dashboard Content */ 
+      <div className="p-6">
+        <h1 className="text-2xl font-bold text-gray-800">
+        Overview 
+        </h1>
+        </div>
+    </section>
     </div>
+  </main>
   )
 }
-
-export default App

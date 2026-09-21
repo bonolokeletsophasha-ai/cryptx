1ece6410c4a52d09ea4743be414c1fb0d946585c
bonolokeletsophasha-ai <bonolokeletsophasha@gmail.com>
2026-09-21 18:26:46 +0200

    debugged

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,29 @@ import Sidebar from './components/layout/Sidebar'
 import Header from './components/layout/Header'
 
 function App() {
-  return ( 
-  <main className="min-h-screen bg-{#858585] p-0 lg:p-4">
-    <div className="mx-auto flex min-h-screen max-w-[1400px] overflow-hidden bg-white lg:min-h-[calc(100vh-2rem)] lg:rounded-2xl">
+  return (
+    <main className="min-h-screen bg-[#858585] p-0 lg:p-4">
+      
+      <div className="mx-auto flex min-h-screen max-w-[1400px] overflow-hidden bg-white lg:min-h-[calc(100vh-2rem)] lg:rounded-2xl">
 
-    /* Sidebar */ 
-    <Sidebar/>
+        <Sidebar />
 
-    /* Main Area*/ 
-    <section className="min-w-0 flex-1">
+        <section className="min-w-0 flex-1">
+          
+          <Header />
 
-      /* Header */
-      <header/>
+          <div className="p-6">
+            <h1 className="text-2xl font-bold text-gray-800">
+              Overview
+            </h1>
+          </div>
 
-      /* Dashboard Content */ 
-      <div className="p-6">
-        <h1 className="text-2xl font-bold text-gray-800">
-        Overview 
-        </h1>
-        </div>
-    </section>
-    </div>
-  </main>
+        </section>
+
+      </div>
+
+    </main>
   )
 }
+
+export default App
